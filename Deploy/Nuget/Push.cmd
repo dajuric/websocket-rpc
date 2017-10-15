94939575adec6ea104fe454eb0643be0ea835930
Darko Jurić <darko.juric2@gmail.com>
2017-10-15 17:49:02 +0200

    Fixing Push

--- Deploy/Nuget/Push.cmd
+++ Deploy/Nuget/Push.cmd
@@ -3,13 +3,13 @@
 timeout /T 5
 
 :: settings
-set nugetPath=%cd%\..\.nuget
+set nugetPath=%cd%\..\..\.nuget
 
 echo.
 echo Pushing packages:
 for /r %%f in (*.nupkg) do (
 	echo   %%f
-	"%nugetPath%\nuget.exe" push "%%f"
+	"%nugetPath%\nuget.exe" push "%%f" -Source https://www.nuget.org/api/v2/package
 )
 
 echo.
