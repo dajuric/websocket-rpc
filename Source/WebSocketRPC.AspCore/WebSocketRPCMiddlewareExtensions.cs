905fd615075fb8e423b6fb645fd0c4c51287c1ad
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-15 14:37:58 +0200

    Introducing `WebSocketsRPCOptions` #11

--- Source/WebSocketRPC.AspCore/WebSocketRPCMiddlewareExtensions.cs
+++ Source/WebSocketRPC.AspCore/WebSocketRPCMiddlewareExtensions.cs
@@ -1,39 +1,35 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System;
 
 namespace WebSocketRPC
-{ 
+{
     /// <summary>
     /// WebSocket RPC middle-ware extensions.
     /// </summary>
     public static class WebSocketRPCMiddlewareExtensions
     {
         /// <summary>
-        /// Branches the request pipeline based on matches of the given request path. If  the request path starts with the given path, the branch is executed.
-        /// <para>Make sure the 'UseWebSockets()' from Microsoft.AspNetCore.WebSockets is called before.</para>
-        /// </summary>
-        /// <param name="app">Application builder.</param>
-        /// <param name="path">The request path to match.</param>
-        /// <param name="onConnect">Action triggered when a new connection is established.</param>
-        /// <returns>Application builder.</returns>
-        public static IApplicationBuilder MapWebSocketRPC(this IApplicationBuilder app,
-                                                          PathString path,
-                                                          Action<HttpContext, Connection> onConnect)
-        {
-            return app.Map(path, (_app) => _app.UseMiddleware<WebSocketRPCMiddleware>(onConnect));
-        }
-
-        /// <summary>
         /// Adds a WebSocketRPC middleware to the application's request pipeline.
-        /// <para>Make sure the 'UseWebSockets()' from Microsoft.AspNetCore.WebSockets is called before.</para>
         /// </summary>
         /// <param name="app">Application builder.</param>
-        /// <param name="onConnect">Action triggered when a new connection is established.</param>
+        /// <param name="options">Options for WebSocketRPC calls.</param>
         /// <returns>Application builder.</returns>
-        public static IApplicationBuilder UseWebSocketRPC(this IApplicationBuilder app, Action<HttpContext, Connection> onConnect)
+        public static IApplicationBuilder UseWebSocketRPC(this IApplicationBuilder app, WebSocketRPCOptions options)
         {
-           return app.UseMiddleware<WebSocketRPCMiddleware>(onConnect);
+            app.UseWebSockets(options);
+
+            foreach (var binding in options.Bindings)
+            {
+                if (string.IsNullOrWhiteSpace(binding.Key))
+                {
+                    app.UseMiddleware<WebSocketRPCMiddleware>(binding.Value);
+                }
+                else
+                {
+                    app.Map(binding.Key, (x) => x.UseMiddleware<WebSocketRPCMiddleware>(binding.Value));
+                }
+            }
+
+            return app;
         }
     }
 }
---
+++ Source/WebSocketRPC.AspCore/WebSocketRPCOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace WebSocketRPC
+{
+    public class WebSocketRPCOptions : WebSocketOptions
+    {
+        public Dictionary<PathString, Action<HttpContext, Connection>> Bindings;
+
+        public WebSocketRPCOptions(Action<HttpContext, Connection> onConnect)
+            : this()
+        {
+            Bindings.Add(string.Empty, onConnect);
+        }
+
+        public WebSocketRPCOptions(PathString path, Action<HttpContext, Connection> onConnect)
+            : this()
+        {
+            Bindings.Add(path, onConnect);
+        }
+
+        private WebSocketRPCOptions()
+        {
+            Bindings = new Dictionary<PathString, Action<HttpContext, Connection>>();
+        }
+    }
+}
